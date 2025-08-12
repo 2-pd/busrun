8d2ddd1b5c881ff88e6e3bf1a20ff431b678eb23
kazuma <>
2025-08-12 05:00:00 +0000

    get_gtfs_realtime.pyを運用データの自動更新に対応
    FossilOrigin-Name: cfc61d5eff3095f963422bfc557020d4af6c0331c41f4039f8c4f0080f021dcc

--- busrun/commands/modules/get_gtfs_realtime.py
+++ busrun/commands/modules/get_gtfs_realtime.py
@@ -61,25 +61,61 @@ def get_agency_gtfs_realtime (mes, main_dir, url):
     
     mes("取得したデータをデータベースに記録しています...")
     
+    diagram_revision = diagram.get_diagram_revision(operation_date)
+    now_datetime = time.strftime("%Y-%m-%d %H:%M:%S")
+    
     try:
         conn = sqlite3.connect(main_dir + "/operation_data.db")
         cur = conn.cursor()
+        
+        cur.execute("SELECT `operation_id`, `assign_order`, `vehicle_name`, `latest_trip_sequence`, `change_datetime` FROM `busrun_data` WHERE `operation_date` = :operation_date ORDER BY `operation_id` ASC, `assign_order` DESC", {"operation_date" : operation_date})
+    
+        operation_data = {}
+        
+        for data_item in cur.fetchall():
+            if data_item[0] in operation_data:
+                continue
+            
+            operation_data[data_item[0]] = { "assign_order" : data_item[1], "vehicle_name" : data_item[2], "latest_trip_sequence" : data_item[3], "change_datetime" : data_item[4] }
         
         for trip_data in feed_dict["entity"]:
             trip_id = trip_data["tripUpdate"]["trip"]["tripId"]
             vehicle_name = trip_data["tripUpdate"]["vehicle"]["label"]
             
-            cur.execute("SELECT COUNT(*) FROM `busrun_operation_logs` WHERE `operation_date` = :operation_date AND `trip_id` = :trip_id", {"operation_date" : operation_date, "trip_id" : trip_data["tripUpdate"]["trip"]["tripId"], "vehicle_name" : trip_data["tripUpdate"]["vehicle"]["label"]})
+            cur.execute("REPLACE INTO `busrun_operation_logs`(`operation_date`, `trip_id`, `vehicle_name`) VALUES (:operation_date, :trip_id, :vehicle_name)", {"operation_date" : operation_date, "trip_id" : trip_data["tripUpdate"]["trip"]["tripId"], "vehicle_name" : trip_data["tripUpdate"]["vehicle"]["label"]})
             
-            if cur.fetchone()[0]:
+            if diagram_revision is None:
                 continue
             
-            cur.execute("INSERT INTO `busrun_operation_logs`(`operation_date`, `trip_id`, `vehicle_name`) VALUES (:operation_date, :trip_id, :vehicle_name)", {"operation_date" : operation_date, "trip_id" : trip_data["tripUpdate"]["trip"]["tripId"], "vehicle_name" : trip_data["tripUpdate"]["vehicle"]["label"]})
+            cur.execute("SELECT `operation_id`, `trip_sequence` FROM `busrun_trips` WHERE `diagram_revision` = :diagram_revision AND `trip_id` = :trip_id", {"diagram_revision" : diagram_revision, "trip_id" : trip_data["tripUpdate"]["trip"]["tripId"]})
+            
+            trip_info = cur.fetchone()
+            if trip_info is None:
+                continue
+            
+            if trip_info[0] in operation_data:
+                if trip_info[1] <= operation_data[trip_info[0]]["latest_trip_sequence"]:
+                    continue
+                
+                if trip_data["tripUpdate"]["vehicle"]["label"] == operation_data[trip_info[0]]["vehicle_name"]:
+                    assign_order = operation_data[trip_info[0]]["assign_order"]
+                    change_datetime = operation_data[trip_info[0]]["change_datetime"]
+                else:
+                    assign_order = operation_data[trip_info[0]]["assign_order"] + 1
+                    change_datetime = now_datetime
+                    
+                    cur.execute("UPDATE `busrun_data` SET `change_datetime` = :change_datetime WHERE `operation_date` = :operation_date AND `operation_id` = :operation_id", {"change_datetime" : change_datetime, "operation_date" : operation_date, "operation_id" : trip_info[0]})
+            else:
+                assign_order = 1
+                change_datetime = now_datetime
+            
+            cur.execute("REPLACE INTO `busrun_data`(`operation_date`, `operation_id`, `assign_order`, `vehicle_name`, `latest_trip_sequence`, `change_datetime`) VALUES (:operation_date, :operation_id, :assign_order, :vehicle_name, :latest_trip_sequence, :change_datetime)", {"operation_date" : operation_date, "operation_id" : trip_info[0], "assign_order" : assign_order, "vehicle_name" : trip_data["tripUpdate"]["vehicle"]["label"], "latest_trip_sequence" : trip_info[1], "change_datetime" : change_datetime})
         
         conn.commit()
         conn.close()
     except:
         mes("データベースへの記録に失敗しました", True)
+        mes(traceback.format_exc())
     else:
         mes("データベースへの記録が完了しました")
     

3f4d02c15d3fd571ce634a74f972688f86141706
kazuma <>
2025-08-17 03:00:00 +0000

    アプリトップページの暫定実装
    FossilOrigin-Name: c56494145d617b154f3690cca2e814448a346f9f098353d5af2a5531046cfaea

--- busrun/main.js
+++ busrun/main.js
@@ -208,6 +208,10 @@ function ajax_post (end_point_name, query_str, callback_func, timeout = 30) {
     ajax_request.send(query_str);
 }
 
+function escape_html (text) {
+    return text.replace(/&/g,"&amp;").replace(/</g,"&lt;").replace(/>/g,"&gt;");
+}
+
 
 function change_title (title_text, url = null) {
     document.getElementsByTagName("title")[0].innerText = title_text;
@@ -234,9 +238,9 @@ function update_instance_info () {
         menu_manual_button_elm.style.display = "none";
     }
     
-    /*if (location.pathname === "/") {
+    if (location.pathname === "/") {
         document.getElementById("splash_screen_instance_name").innerText = instance_info["instance_name"];
-    }*/
+    }
 }
 
 (function () {
@@ -273,9 +277,137 @@ function update_instance_info () {
 }());
 
 
+var splash_screen_elm = document.getElementById("splash_screen");
+
+
+var agencies = null;
+
+function get_agency_list (callback_func) {
+    if (agencies !== null) {
+        callback_func(agencies, true);
+        
+        return;
+    }
+    
+    var cache_json = localStorage.getItem("busrun_agencies_cache");
+    
+    if (cache_json !== null) {
+        agencies = JSON.parse(cache_json);
+    } else {
+        agencies = {
+            agencies : {},
+            categories : [],
+            last_modified_timestamp : 0
+        };
+    }
+    
+    if (navigator.onLine) {
+        if (agencies["categories"].length >= 1) {
+            callback_func(agencies, false);
+        }
+        
+        ajax_post("agencies.php", "last_modified_timestamp=" + agencies["last_modified_timestamp"], function (response, last_modified) {
+            if (response !== false && response !== "NO_UPDATES_AVAILABLE") {
+                agencies = JSON.parse(response);
+                
+                var last_modified_date = new Date(last_modified);
+                agencies["last_modified_timestamp"] = Math.floor(last_modified_date.getTime() / 1000);
+                
+                localStorage.setItem("busrun_agencies_cache", JSON.stringify(agencies));
+            }
+            
+            callback_func(agencies, true);
+        }, 10);
+    } else {
+        callback_func(agencies, true);
+    }
+}
+
+var agency_list_area;
+var agency_links_area;
+
+function update_agency_list (agencies, area_elm = null, loading_completed = true) {
+    if (area_elm !== null) {
+        agency_list_area = area_elm;
+    } else {
+        area_elm = agency_list_area;
+    }
+    
+    var categories_html = "";
+    var agency_links_html = "";
+    var heading_cnt = 0;
+    
+    for (var category of agencies["categories"]) {
+        var category_html = " style='color: " + category["category_color"] + ";'><b>" + escape_html(category["category_name"]) + "</b></";
+        categories_html += "<li class='category_index' onclick='scroll_to_category(" + heading_cnt + ");'" + category_html + "li>";
+        agency_links_html += "<h3 class='agencies_heading'" + category_html + "h3>";
+        
+        heading_cnt++;
+        for (var agency_id of category["agencies"]) {
+            var css_code = "background-color: " + agencies["agencies"][agency_id]["base_color"] + ";";
+            if ("color_stripes" in agencies["agencies"][agency_id]) {
+                var gradient_code = "";
+                for (var stripe_data of agencies["agencies"][agency_id]["color_stripes"]) {
+                    gradient_code += (gradient_code.length >= 1 ? "," : "") + " linear-gradient(to bottom, transparent 0% " + stripe_data["start"] + "%, " + stripe_data["color"] + " " + stripe_data["start"] + "% " + stripe_data["end"] + "%, transparent " + stripe_data["end"] + "% 100%)";
+                }
+                
+                css_code += " background-image:" + gradient_code + ";";
+            }
+            
+            agency_links_html += "<a href='/agency_" + agency_id + "/' onclick='event.preventDefault(); select_agency(\"" + agency_id + "\");' style='" + css_code + ";'><span>" + escape_html(agencies["agencies"][agency_id]["agency_name"]) + "</span></a>";
+        }
+    }
+    
+    if (!loading_completed) {
+        categories_html += "<div class='loading_icon'></div>";
+    } else if (agencies["categories"].length === 0) {
+        agency_links_html = "<div class='no_data'>利用可能なデータがありません</div>";
+    }
+    
+    area_elm.innerHTML = "<ul id='category_area'>" + categories_html + "</ul><div id='agency_links_area' onscroll='agency_links_area_onscroll();'>" + agency_links_html + "</div>";
+    
+    splash_screen_elm.className = "splash_screen_loaded";
+    
+    agency_links_area = document.getElementById("agency_links_area");
+    
+    agency_links_area_onscroll();
+}
+
+function scroll_to_category (index_val) {
+    agency_links_area.scrollTop += document.getElementsByClassName("agencies_heading")[index_val].getBoundingClientRect().top - agency_links_area.getBoundingClientRect().top - 5;
+}
+
+function agency_links_area_onscroll () {
+    var heading_elms = document.getElementsByClassName("agencies_heading");
+    var index_elms = document.getElementById("category_area").getElementsByTagName("li");
+    
+    var agency_links_area_top = agency_links_area.getBoundingClientRect().top;
+    
+    for (var cnt = 0; cnt < heading_elms.length; cnt++) {
+        if (heading_elms[cnt].getBoundingClientRect().top > agency_links_area_top) {
+            index_elms[cnt].classList.add("active_index");
+            
+            break;
+        }
+        
+        index_elms[cnt].classList.remove("active_index");
+    }
+    
+    for (cnt++; cnt < heading_elms.length; cnt++) {
+        index_elms[cnt].classList.remove("active_index");
+    }
+}
+
+
 window.onload = function () {
     db_open_promise.then(function () {
-        
+        if (location.pathname.startsWith("/agency_")) {
+            
+        } else {
+            get_agency_list(function (agencies, loading_completed) {
+                update_agency_list(agencies, document.getElementById("splash_screen_inner"), loading_completed);
+            });
+        }
     });
 };
 
